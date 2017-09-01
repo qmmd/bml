19351eef41f599ecacb912f0fe1322bc4b159938
Nicolas Bock <nicolasbock@gmail.com>
2017-09-01 09:47:02 -0600

    Add `bml_convert` API
    The new API (proposed in #90) converts a bml matrix from one type to
    another. This change adds conversion to the dense bml matrix type.

---
+++ src/C-interface/dense/bml_convert_dense_typed.c
@@ -0,0 +1,28 @@
+#include "../macros.h"
+#include "../typed.h"
+#include "bml_allocate_dense.h"
+#include "bml_getters.h"
+#include "bml_introspection.h"
+#include "bml_types_dense.h"
+
+#include <complex.h>
+
+bml_matrix_dense_t *TYPED_FUNC(
+    bml_convert_dense) (
+    const bml_matrix_t * A,
+    const bml_distribution_mode_t distrib_mode)
+{
+    int N = bml_get_N(A);
+    bml_matrix_dense_t *B = bml_zero_matrix_dense(0, N, distrib_mode);
+    REAL_T *Bij = (REAL_T *) B->matrix;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            Bij[ROWMAJOR(i, j, N, N)] = *(REAL_T *) bml_get(A, i, j);
+        }
+    }
+
+    return B;
+}
