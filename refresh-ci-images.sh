6673a485ee5ba5cc0302d204e8c31120edeb9fc7
Nicolas Bock <nicolasbock@gmail.com>
2020-02-13 11:20:47 -0700

    Add second image for lint workflow
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- refresh-ci-images.sh
+++ refresh-ci-images.sh
@@ -5,5 +5,7 @@ set -x
 : ${IMAGE_TAG:=bml-ci}
 : ${IMAGE_VERSION:=1.0}
 
-docker build --tag nicolasbock/${IMAGE_TAG}:${IMAGE_VERSION} ci-images
-docker push nicolasbock/${IMAGE_TAG}:${IMAGE_VERSION}
+for workflow in build lint; do
+  docker build --tag nicolasbock/${IMAGE_TAG}-${workflow}:${IMAGE_VERSION} ci-images/${workflow}
+  docker push nicolasbock/${IMAGE_TAG}-${workflow}:${IMAGE_VERSION}
+done
