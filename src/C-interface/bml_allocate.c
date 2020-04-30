51d6d2b51d846193dc0796dab08e5d8fa58c61cd
toks ade <adedoyin.adetokunbo@gmail.com>
2020-04-30 13:31:07 -0600

    Update bml_allocate.c
    modified HAVE_POSIX_MEMALIGN macro for first touch

--- src/C-interface/bml_allocate.c
+++ src/C-interface/bml_allocate.c
@@ -54,6 +54,7 @@ bml_allocate_memory(
 #elif defined(HAVE_POSIX_MEMALIGN)
     char *ptr;
     posix_memalign((void **) &ptr, MALLOC_ALIGNMENT, size);
+    #pragma omp parallel for simd
     for (size_t i = 0; i < size; i++)
     {
         ptr[i] = 0;
