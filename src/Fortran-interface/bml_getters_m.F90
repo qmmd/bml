b1d4e978d310fe7babcd2dec26ae2b18d7e00777
Mike Wall <mewall@jlselogin2.ftm.alcf.anl.gov>
2020-05-05 17:32:07 +0000

    Fix broken tests on ANL jlse

--- src/Fortran-interface/bml_getters_m.F90
+++ src/Fortran-interface/bml_getters_m.F90
@@ -41,7 +41,7 @@ contains
     ptr = bml_get_diagonal_C(a%ptr)
     call c_f_pointer(ptr, diagonal_ptr, [bml_get_N(a)])
     diagonal = diagonal_ptr
-    deallocate(diagonal_ptr)
+    ! deallocate(diagonal_ptr)
 
   end subroutine bml_get_diagonal_single_real
 
@@ -60,7 +60,7 @@ contains
     ptr = bml_get_diagonal_C(a%ptr)
     call c_f_pointer(ptr, diagonal_ptr, [bml_get_N(a)])
     diagonal = diagonal_ptr
-    deallocate(diagonal_ptr)
+    ! deallocate(diagonal_ptr)
 
   end subroutine bml_get_diagonal_double_real
 
@@ -79,7 +79,7 @@ contains
     ptr = bml_get_diagonal_C(a%ptr)
     call c_f_pointer(ptr, diagonal_ptr, [bml_get_N(a)])
     diagonal = diagonal_ptr
-    deallocate(diagonal_ptr)
+    ! deallocate(diagonal_ptr)
 
   end subroutine bml_get_diagonal_single_complex
 
@@ -98,7 +98,7 @@ contains
     ptr = bml_get_diagonal_C(a%ptr)
     call c_f_pointer(ptr, diagonal_ptr, [bml_get_N(a)])
     diagonal = diagonal_ptr
-    deallocate(diagonal_ptr)
+    ! deallocate(diagonal_ptr)
 
   end subroutine bml_get_diagonal_double_complex
 
@@ -121,7 +121,7 @@ contains
     ptr = bml_get_row_C(a%ptr, i - 1)
     call c_f_pointer(ptr, row_ptr, [bml_get_N(a)])
     row = row_ptr
-    deallocate(row_ptr)
+    ! deallocate(row_ptr)
 
   end subroutine bml_get_row_single_real
 
@@ -142,7 +142,7 @@ contains
     ptr = bml_get_row_C(a%ptr, i - 1)
     call c_f_pointer(ptr, row_ptr, [bml_get_N(a)])
     row = row_ptr
-    deallocate(row_ptr)
+    ! deallocate(row_ptr)
 
   end subroutine bml_get_row_double_real
 
@@ -163,7 +163,7 @@ contains
     ptr = bml_get_row_C(a%ptr, i - 1)
     call c_f_pointer(ptr, row_ptr, [bml_get_N(a)])
     row = row_ptr
-    deallocate(row_ptr)
+    ! deallocate(row_ptr)
 
   end subroutine bml_get_row_single_complex
 
@@ -184,7 +184,7 @@ contains
     ptr = bml_get_row_C(a%ptr, i - 1)
     call c_f_pointer(ptr, row_ptr, [bml_get_N(a)])
     row = row_ptr
-    deallocate(row_ptr)
+    ! deallocate(row_ptr)
 
   end subroutine bml_get_row_double_complex
 
