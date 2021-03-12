32bcb4179da442934ffaf86710bb016f584a24bd
Jean-Luc Fattebert <fattebertj@ornl.gov>
2021-03-12 14:57:25 -0500

    Minor fixes

--- src/C-interface/distributed2d/bml_multiply_distributed2d_typed.c
+++ src/C-interface/distributed2d/bml_multiply_distributed2d_typed.c
@@ -5,20 +5,16 @@
 #include "../bml_logger.h"
 #include "../bml_copy.h"
 #include "../bml_parallel.h"
-#include "../bml_getters.h"
 
 #include "bml_allocate_distributed2d.h"
-#include "bml_import_distributed2d.h"
 #include "bml_types_distributed2d.h"
 
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
 #include <assert.h>
 #include <mpi.h>
 #include <stdio.h>
 
-/** Matrix multiply.
+/** Matrix multiply using Cannon's algorithm.
  *
  * C = alpha * A * B + beta * C
  *
@@ -41,15 +37,11 @@ void TYPED_FUNC(
     double threshold)
 {
     // make a copy of A and B local submatrices
-    bml_matrix_t *Atmp1 = NULL;
-    bml_matrix_t *Atmp2 = NULL;
-    Atmp1 = bml_copy_new(A->matrix);
-    Atmp2 = bml_copy_new(A->matrix);
+    bml_matrix_t *Atmp1 = bml_copy_new(A->matrix);
+    bml_matrix_t *Atmp2 = bml_copy_new(A->matrix);
 
-    bml_matrix_t *Btmp1 = NULL;
-    bml_matrix_t *Btmp2 = NULL;
-    Btmp1 = bml_copy_new(B->matrix);
-    Btmp2 = bml_copy_new(B->matrix);
+    bml_matrix_t *Btmp1 = bml_copy_new(B->matrix);
+    bml_matrix_t *Btmp2 = bml_copy_new(B->matrix);
 
     // shift all submatrices A(i,j) to the left by i steps
     if (A->myprow > 0)
@@ -59,6 +51,7 @@ void TYPED_FUNC(
         bml_mpi_send(Atmp1, dst, A->comm);
         bml_mpi_recv(Atmp2, src, A->comm);
     }
+
     // shift all submatrices B(i,j) up by j steps
     if (B->mypcol > 0)
     {
@@ -69,33 +62,27 @@ void TYPED_FUNC(
     }
 
     // perform local submatrices multiplication
-//{
-//REAL_T *val = bml_get_element(Atmp2,0,0);
-//printf("A: %d %d %le\n", A->myprow, A->mypcol, *val);
-//}
-//{
-//REAL_T *val = bml_get_element(Btmp2,0,0);
-//printf("B: %d %d %le\n", A->myprow, A->mypcol, *val);
-//}
     bml_multiply(Atmp2, Btmp2, C->matrix, alpha, beta, threshold);
-#if 1
+
     for (int k = 1; k < C->npcols; k++)
     {
-        //swap pointers
+        // swap pointers to have latest matrices in tmp1
         bml_matrix_t *tmp = Atmp1;
         Atmp1 = Atmp2;
         Atmp2 = tmp;
         tmp = Btmp1;
         Btmp1 = Btmp2;
         Btmp2 = tmp;
-        // Move submatrices A(i,j) one step to left
+
+        // Move local submatrices one step to left
         {
             int src, dst;
             MPI_Cart_shift(A->comm, 1, -1, &src, &dst);
             bml_mpi_send(Atmp1, dst, A->comm);
             bml_mpi_recv(Atmp2, src, A->comm);
         }
-        // Move submatrices B(i,j) one step up
+
+        // Move local submatrices one step up
         {
             int src, dst;
             MPI_Cart_shift(B->comm, 0, -1, &src, &dst);
@@ -106,7 +93,7 @@ void TYPED_FUNC(
         // perform local submatrices multiplication
         bml_multiply(Atmp2, Btmp2, C->matrix, alpha, 1., threshold);
     }
-#endif
+
     bml_deallocate(&Atmp1);
     bml_deallocate(&Atmp2);
     bml_deallocate(&Btmp1);
